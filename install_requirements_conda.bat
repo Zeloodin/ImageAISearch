9328c7663bb4ec9d4c471b9e011b5554b6bf26e2
Roman Magafurov <roman.mag1997@yandex.ru>
2024-09-23 12:48:18 +0500

    удаление лишних файлов, для упрощения

--- install_requirements_conda.bat
+++
@@ -1,5 +0,0 @@
-chcp 65001
-installer_files\env\python.exe -m pip install --upgrade pip
-installer_files\env\Scripts\pip install -r requirements.txt
-installer_files\env\Scripts\pip install ftfy regex tqdm
-installer_files\env\Scripts\pip install git+https://github.com/openai/CLIP.git
--- run_conda.bat
+++
@@ -1,3 +0,0 @@
-chcp 65001
-installer_files\env\python.exe run.py
-pause
